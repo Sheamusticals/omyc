6f8d377d32d438d60cf0689608433da0ba649cb4
Francis Antwi <sheamusticals@gmail.com>
2024-11-19 18:53:03 +0000

    :update

--- omyc/settings.py
+++ omyc/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ec$32@e0uhf3n=!al=c@al4z)_4_-tipu3np74vdesjan97)5!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = [
